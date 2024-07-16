600d03ef8cc97a1880d45691fbaf46ac57b86a70
Pane <pane4846@gmail.com>
2024-07-16 19:54:31 +0900

    [FEAT]검색 카테고리그룹 이미지 추가

--- What?fle/View/Module/Add/SelectLocation/Entity/SelectLocationModel.swift
+++ What?fle/View/Module/Add/SelectLocation/Entity/SelectLocationModel.swift
@@ -5,7 +5,7 @@
 //  Created by 이정환 on 3/1/24.
 //
 
-import Foundation
+import UIKit
 
 struct KakaoSearchModel: Decodable {
     let meta: KakaoSearchMetaModel
@@ -43,6 +43,47 @@ struct KakaoSearchSameNameModel: Decodable {
     }
 }
 
+enum CategoryGroupCode: String, Decodable {
+    case mt1 = "MT1"
+    case cs2 = "CS2"
+    case sc4 = "SC4"
+    case ac5 = "AC5"
+    case pk6 = "PK6"
+    case ol7 = "O17"
+    case sw8 = "SW8"
+    case bk9 = "BK9"
+    case ad5 = "AD5"
+    case fd6 = "FD6"
+    case ce7 = "CE7"
+    case hp8 = "HP8"
+    case pm9 = "PM9"
+    case unknown
+
+    var image: UIImage? {
+        switch self {
+        case .mt1: return .cart
+        case .cs2: return .store
+        case .sc4, .ac5: return .school
+        case .pk6: return .parking
+        case .ol7: return .vehicle
+        case .sw8: return .train
+        case .bk9: return .bank
+        case .ad5: return .lodging
+        case .fd6: return .food
+        case .ce7: return .cafe
+        case .hp8: return .hospital
+        case .pm9: return .pharmacy
+        default: return .placeholdCell
+        }
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let rawValue = try container.decode(String.self)
+        self = CategoryGroupCode(rawValue: rawValue) ?? .unknown
+    }
+}
+
 struct KakaoSearchDocumentsModel: Decodable, Equatable {
     let placeName: String
     let distance: String
@@ -52,7 +93,7 @@ struct KakaoSearchDocumentsModel: Decodable, Equatable {
     let roadAddressName: String
     let id: String
     let phone: String
-    let categoryGroupCode: String
+    let categoryGroupCode: CategoryGroupCode
     let categoryGroupName: String
     let longitudeX: String
     let latitudeY: String
@@ -71,11 +112,11 @@ struct KakaoSearchDocumentsModel: Decodable, Equatable {
         case longitudeX = "x"
         case latitudeY = "y"
     }
-    
+
     var longitude: Double {
         Double(longitudeX) ?? 0.0
     }
-    
+
     var latitude: Double {
         Double(latitudeY) ?? 0.0
     }
